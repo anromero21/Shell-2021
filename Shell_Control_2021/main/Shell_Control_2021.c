66a893e988cde8c37d283215c0b5b5fbc04a3de7
anromero21 <60559405+anromero21@users.noreply.github.com>
2021-01-31 15:38:38 -0600

    .

--- Shell_Control_2021/main/Shell_Control_2021.c
+++ Shell_Control_2021/main/Shell_Control_2021.c
@@ -2,7 +2,9 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "driver/gpio.h"
+#include "driver/uart.h"
 #include "sdkconfig.h"
+#include "string.h"
 /*
 #include <ESP32_ExtInterrupt.h>
 #include <PWM_ESP32.h>
@@ -43,13 +45,13 @@ const char *password = "2805042113";
 //*********** DEFINITIONS ***********
 //***********************************
 // INPUT    D-I
-#define DIRO GPIO_NUM_36 // VP
-#define FF2 GPIO_NUM_39  // VN
-#define FF1 GPIO_NUM_34
-#define TACHO GPIO_NUM_35
-#define CSOUT GPIO_NUM_32
+#define DIRO (GPIO_NUM_36) // VP    // IT
+#define FF2 (GPIO_NUM_39)  // VN    // IT
+#define FF1 (GPIO_NUM_34)           // IT
+#define TACHO (GPIO_NUM_35)         // IT
+#define CSOUT (GPIO_NUM_32)
 
-// BUTTONS    I-D
+// BUTTONS    I-D       // 2da ESP
 #define B_DIR GPIO_NUM_33
 #define B_COAST GPIO_NUM_25
 #define B_BRAKE GPIO_NUM_26
@@ -57,15 +59,19 @@ const char *password = "2805042113";
 #define B_RESET GPIO_NUM_14
 
 // OUTPUT
-#define COAST GPIO_NUM_23
-#define ESF GPIO_NUM_22
-#define RST GPIO_NUM_21
-#define BRAKE GPIO_NUM_19
-#define DIR GPIO_NUM_18
-#define PWM_PIN GPIO_NUM_5
-#define MODE GPIO_NUM_17
-#define REF GPIO_NUM_4
-#define VDSTH GPIO_NUM_0
+#define COAST (GPIO_NUM_23)
+#define ESF (GPIO_NUM_22)
+#define RST (GPIO_NUM_21)
+#define BRAKE (GPIO_NUM_19)
+#define DIR (GPIO_NUM_18)
+#define PWM_PIN (GPIO_NUM_5)    // PWM
+#define MODE (GPIO_NUM_25)
+#define REF (GPIO_NUM_4)        // PWM
+#define VDSTH (GPIO_NUM_0)      // PWM
+
+// COMMUNICATION
+#define TXD_PIN_2 (GPIO_NUM_17)
+#define RXD_PIN_2 (GPIO_NUM_16)
 
 // CONSTANTS
 #define AV 19
@@ -74,6 +80,10 @@ const char *password = "2805042113";
 #define RT 200000    // 200kohms
 #define DELTA_ANGLE 0.0951998
 #define CONVERSION 9.5493
+
+// BIT-MASK
+#define GPIO_I_BIT_MASK  ((1ULL<<CSOUT)  
+#define GPIO_O_BIT_MASK  ((1ULL<<COAST) | (1ULL<<ESF)) | (1ULL<<RST)) | (1ULL<<BRAKE)) | (1ULL<<DIR)) | (1ULL<<MODE))  
 
 const char *mqttServer = "broker.mqttdashboard.com";
 const int mqttPort = 8000;
@@ -133,7 +143,7 @@ String fault;
 //***********************************
 //*************** RTOS **************
 //***********************************
-TaskHandle_t Task1, Task2, Task3, Task4, Task5;
+TaskHandle_t Task1, Task2, Task3, Task4, Task5, Task6, Task7;
 
 //***********************************
 //************ FUNCTIONS ************
@@ -144,6 +154,7 @@ void speedDir(void *parameter);
 void currentControl(void *parameter);
 void buttons(void *parameter);
 void communication(void *parameter);
+void lora(void *parameter);
 
 void setup_wifi();
 void callback(char *topic, byte *payload, unsigned int length);
@@ -154,24 +165,27 @@ void app_main(void)
     gpio_pad_select_gpio(BLINK_GPIO);
     gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
 
-    gpio_pad_select_gpio(DIR);
-    gpio_pad_select_gpio(RST);
-    gpio_pad_select_gpio(MODE);
-    gpio_pad_select_gpio(COAST);
-    gpio_pad_select_gpio(BRAKE);
-    gpio_pad_select_gpio(ESF);
-    gpio_pad_select_gpio(CSOUT);
-    gpio_pad_select_gpio(DIRO);
+    gpio_config_t i_conf;
+    i_conf.intr_type = GPIO_PIN_INTR_DISABLE;
+    i_conf.mode = GPIO_MODE_INPUT;
+    i_conf.pin_bit_mask = GPIO_I_BIT_MASK;
+    i_conf.pull_down_en = GPIO_PULLDOWN_DISABLE;
+    i_conf.pull_up_en = GPIO_PULLUP_DISABLE;
+    esp_err_t error=gpio_config(&i_conf);
+    if(error!=ESP_OK){
+        printf("Error configuring inputs \n");
+    }
 
-    gpio_set_direction(RST, GPIO_MODE_OUTPUT);
-    gpio_set_direction(DIR, GPIO_MODE_OUTPUT);
-    gpio_set_direction(MODE, GPIO_MODE_OUTPUT);
-    gpio_set_direction(COAST, GPIO_MODE_OUTPUT);
-    gpio_set_direction(BRAKE, GPIO_MODE_OUTPUT);
-    gpio_set_direction(ESF, GPIO_MODE_OUTPUT);
-    gpio_set_direction(CSOUT, GPIO_MODE_INPUT);
-    gpio_set_direction(DIRO, GPIO_MODE_INPUT);
-    // gpio_set_direction(POT, GPIO_MODE_INPUT);
+    gpio_config_t o_conf;
+    o_conf.intr_type = GPIO_PIN_INTR_DISABLE;
+    o_conf.mode = GPIO_MODE_OUTPUT;
+    o_conf.pin_bit_mask = GPIO_O_BIT_MASK;
+    o_conf.pull_down_en = GPIO_PULLDOWN_DISABLE;
+    o_conf.pull_up_en = GPIO_PULLUP_DISABLE;
+    esp_err_t error=gpio_config(&o_conf);
+    if(error!=ESP_OK){
+        printf("Error configuring outputs \n");
+    }
 
     pwmPin.setup(PWM_PIN, 1, 15000, 12, 1);
     vdsthPin.setup(VDSTH, 2, 15000, 12, 1);
@@ -190,7 +204,8 @@ void app_main(void)
     xTaskCreatePinnedToCore(speedDir, "Task 3", 1000, NULL, 1, &Task3, 1);
     xTaskCreatePinnedToCore(currentControl, "Task 4", 1000, NULL, 1, &Task4, 1);
     xTaskCreatePinnedToCore(buttons, "Task_5", 1000, NULL, 2, &Task5, 0);
-    xTaskCreatePinnedToCore(communication, "Task_5", 1000, NULL, 2, &Task6, 0);
+    xTaskCreatePinnedToCore(communication, "Task_6", 1000, NULL, 2, &Task6, 0);
+    xTaskCreatePinnedToCore(lora, "Task_7", 1000, NULL, 2, &Task7, 0);
 
     gpio_set_level(DIR, 1);
     gpio_set_level(ESF, 1);
@@ -198,155 +213,204 @@ void app_main(void)
 
     tripCurrent = 15;
 
+    static const int RX_BUF_SIZE = 1024;
+
+    uart_config_t uart_config = {
+        .baud_rate = 115200,
+        .data_bits = UART_DATA_8_BITS,
+        .parity = UART_PARITY_DISABLE,
+        .stop_bits = UART_STOP_BITS_1,
+        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+        .source_clk = UART_SCLK_APB,
+    };
+
+    uart_driver_install(UART_NUM_2, RX_BUF_SIZE * 2, 0, 0, NULL, 0);
+    uart_param_config(UART_NUM_2, &uart_config);
+    uart_set_pin(UART_NUM_2, TXD_PIN_2, RXD_PIN_2, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
+}
+
+//***********************************
+//************* BUTTONS *************
+//***********************************
+void buttons(void *parameters)
+{
     while (1)
     {
-        printf("Turning off the LED\n");
-        gpio_set_level(BLINK_GPIO, 0);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-        printf("Turning on the LED\n");
-        gpio_set_level(BLINK_GPIO, 1);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-    }
+        vdsthPin.setDuty(30);
 
-    //***********************************
-    //************* BUTTONS *************
-    //***********************************
-    void buttons(void *parameters)
-    {
-        while (1)
-        {
-            vdsthPin.setDuty(30);
+        bool dReset = resetInterrupt.getState();
+        bool dDir = dirInterrupt.getState();
+        bool dCoast = coastInterrupt.getState();
+        bool dBrake = coastInterrupt.getState();
 
-            bool dReset = resetInterrupt.getState();
-            bool dDir = dirInterrupt.getState();
-            bool dCoast = coastInterrupt.getState();
-            bool dBrake = coastInterrupt.getState();
+        printf("DIR: %d  ", dDir);
+        printf("COAST: %d  ", dCoast);
+        printf("BRAKE: %d  ", dBrake);
+        printf("RESET: %d  ", dReset);
 
-            printf("DIR: %d  ", dDir);
-            printf("COAST: %d  ", dCoast);
-            printf("BRAKE: %d  ", dBrake);
-            printf("RESET: %d  ", dReset);
+        // gpio_set_level(DIR, 1);
+        gpio_set_level(COAST, dCoast);
+        gpio_set_level(BRAKE, dBrake);
+        gpio_set_level(RST, HIGH);
 
-            // gpio_set_level(DIR, 1);
-            gpio_set_level(COAST, dCoast);
-            gpio_set_level(BRAKE, dBrake);
-            gpio_set_level(RST, HIGH);
-
-            vTaskDelay(10 / portTICK_PERIOD_MS);
-        }
-    }
-
-    //***********************************
-    //************ SPEED-DIR ************
-    //***********************************
-    void speedDir(void *parameter)
-    {
-        while (1)
-        {
-            if (tachoInterrupt.available() && dirInterrupt.available())
-            {
-                bool direction = dirInterrupt.getState();
-                float time = tachoInterrupt.getElapsedMicros();
-                motorRads = DELTA_ANGLE / (time / 1000000);
-                rpm = (motorRads * CONVERSION); //60.0f) / 360.0f;
-
-                gpio_set_level(DIR, direction);
-
-                printf("RPM: %0.2f", rpm);
-            }
-
-            vTaskDelay(10 / portTICK_PERIOD_MS);
-        }
-    }
-
-    //***********************************
-    //************* SENSING *************
-    //***********************************
-    void sensing(void *parameter)
-    {
-        while (1)
-        {
-            csoutDividerVoltage = analogRead(CSOUT); // 3.3V/4096
-            printf("CSOUT DIVIDER VOLTAGE: %d", csoutDividerVoltage);
-
-            csoutVoltage = (3.3 * csoutDividerVoltage / 4096) * 3.2 / 2.2; // Divisor de voltaje
-            printf("CSOUT: %d", csoutVoltage);
-
-            loadCurrent = (csoutVoltage - VOOS) / (AV * RSENSE);
-            printf("LOAD CURRENT: %d", loadCurrent);
-
-            vTaskDelay(10 / portTICK_PERIOD_MS);
-        }
-    }
-
-    //***********************************
-    //********* CURRENT CONTROL *********
-    //***********************************
-    void currentControl(void *parameter)
-    {
-        while (1)
-        {
-            // int lec = analogRead(POT); // 3.3V/4096
-            // int conv = map(lec, 0, 4096, 0, 100);
-
-            refVoltage = tripCurrent * (RSENSE * AV) + VOOS;
-
-            dutyRef = refVoltage * 100 / 3.3;
-            
-            if (dutyRef > 100)
-                dutyRef = 100;
-
-            pwmPin.setDuty(100); // 100-Torque Control     0-Current recirculate
-            refPin.setDuty(dutyRef);
-
-            vTaskDelay(10 / portTICK_PERIOD_MS);
-        }
-    }
-
-    //***********************************
-    //************** FAULTS *************
-    //***********************************
-    void faults(void *parameter)
-    {
-        while (1)
-        {
-            int l1 = FF1Interrupt.getState();
-            int l2 = FF2Interrupt.getState();
-
-            if (l1 == 1 && l2 == 1)
-                fault = "No fault";
-
-            else if (l1 == 0 && l2 == 0)
-            {
-                fault = "Undervoltage, Overtemperature or Logic fault";
-                printf("Undervoltage, Overtemperature or Logic fault");
-            }
-            else if (l1 == 1 && l2 == 0)
-            {
-                fault = "Short to ground, short to supply or shorted motor winding";
-                printf("Short to ground, short to supply or shorted motor winding");
-            }
-            else if (l1 == 0 && l2 == 1)
-            {
-                fault = "Low load current";
-                printf("Low load current");
-            }
-
-            vTaskDelay(10 / portTICK_PERIOD_MS);
-        }
-    }
-
-    //***********************************
-    //********** COMMUNICATION **********
-    //***********************************
-    void communication(void *parameter)
-    {
-        while (1)
-        {
-            
-        }
+        vTaskDelay(10 / portTICK_PERIOD_MS);
     }
 }
+
+//***********************************
+//************ SPEED-DIR ************
+//***********************************
+void speedDir(void *parameter)
+{
+    while (1)
+    {
+        if (tachoInterrupt.available() && dirInterrupt.available())
+        {
+            bool direction = dirInterrupt.getState();
+            float time = tachoInterrupt.getElapsedMicros();
+            motorRads = DELTA_ANGLE / (time / 1000000);
+            rpm = (motorRads * CONVERSION); //60.0f) / 360.0f;
+
+            gpio_set_level(DIR, direction);
+
+            printf("RPM: %0.2f", rpm);
+        }
+
+        vTaskDelay(10 / portTICK_PERIOD_MS);
+    }
+}
+
+//***********************************
+//************* SENSING *************
+//***********************************
+void sensing(void *parameter)
+{
+    while (1)
+    {
+        csoutDividerVoltage = analogRead(CSOUT); // 3.3V/4096
+        printf("CSOUT DIVIDER VOLTAGE: %d", csoutDividerVoltage);
+
+        csoutVoltage = (3.3 * csoutDividerVoltage / 4096) * 3.2 / 2.2; // Divisor de voltaje
+        printf("CSOUT: %d", csoutVoltage);
+
+        loadCurrent = (csoutVoltage - VOOS) / (AV * RSENSE);
+        printf("LOAD CURRENT: %d", loadCurrent);
+
+        vTaskDelay(10 / portTICK_PERIOD_MS);
+    }
+}
+
+//***********************************
+//********* CURRENT CONTROL *********
+//***********************************
+void currentControl(void *parameter)
+{
+    while (1)
+    {
+        // int lec = analogRead(POT); // 3.3V/4096
+        // int conv = map(lec, 0, 4096, 0, 100);
+
+        refVoltage = tripCurrent * (RSENSE * AV) + VOOS;
+
+        dutyRef = refVoltage * 100 / 3.3;
+
+        if (dutyRef > 100)
+            dutyRef = 100;
+
+        pwmPin.setDuty(100); // 100-Torque Control     0-Current recirculate
+        refPin.setDuty(dutyRef);
+
+        vTaskDelay(10 / portTICK_PERIOD_MS);
+    }
+}
+
+//***********************************
+//************** FAULTS *************
+//***********************************
+void faults(void *parameter)
+{
+    while (1)
+    {
+        int l1 = FF1Interrupt.getState();
+        int l2 = FF2Interrupt.getState();
+
+        if (l1 == 1 && l2 == 1)
+            fault = "No fault";
+
+        else if (l1 == 0 && l2 == 0)
+        {
+            fault = "Undervoltage, Overtemperature or Logic fault";
+            printf("Undervoltage, Overtemperature or Logic fault");
+        }
+        else if (l1 == 1 && l2 == 0)
+        {
+            fault = "Short to ground, short to supply or shorted motor winding";
+            printf("Short to ground, short to supply or shorted motor winding");
+        }
+        else if (l1 == 0 && l2 == 1)
+        {
+            fault = "Low load current";
+            printf("Low load current");
+        }
+
+        vTaskDelay(10 / portTICK_PERIOD_MS);
+    }
+}
+
+//***********************************
+//********** COMMUNICATION **********
+//***********************************
+void communication(void *parameter)
+{
+    while (1)
+    {
+        vTaskDelay(10 / portTICK_PERIOD_MS);
+    }
+}
+
+//***********************************
+//************** LORA ***************
+//***********************************
+void lora(void *parameter)
+{
+    while (1)
+    {
+        vTaskDelay(10 / portTICK_PERIOD_MS);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //***********************************
 //************** SETUP **************
